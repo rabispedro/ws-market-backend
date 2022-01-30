5bd9ca277a791b02fb34ac7159ba7fe30195fd4b
Pedro Rabis <rabis.pedro@gmail.com>
2022-01-30 18:06:27 -0300

    :sparkles: Projeto populado com Categorias

--- src/main/java/com/wsmarket/wsmarketbackend/WsMarketBackendApplication.java
+++ src/main/java/com/wsmarket/wsmarketbackend/WsMarketBackendApplication.java
@@ -1,13 +1,32 @@
 package com.wsmarket.wsmarketbackend;
 
+import java.util.Arrays;
+
+import com.wsmarket.wsmarketbackend.domain.Categoria;
+import com.wsmarket.wsmarketbackend.repositories.CategoriaRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WsMarketBackendApplication {
+import lombok.AllArgsConstructor;
 
+@SpringBootApplication
+@AllArgsConstructor
+public class WsMarketBackendApplication implements CommandLineRunner {
+
+	final private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WsMarketBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
