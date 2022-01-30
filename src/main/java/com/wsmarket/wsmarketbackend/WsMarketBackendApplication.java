955d8bbfeafa9e4c640c755a6125dad132f2d53f
Pedro Rabis <rabis.pedro@gmail.com>
2022-01-30 15:51:20 -0300

    :tada: Setup projeto

---
+++ src/main/java/com/wsmarket/wsmarketbackend/WsMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wsmarket.wsmarketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMarketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsmarket/wsmarketbackend/WsMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsmarket.wsmarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
